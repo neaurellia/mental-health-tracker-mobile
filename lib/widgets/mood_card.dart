18a40cfc9a1f1eb539bbc2671119fec33931aa7d
neaurellia <catherineaurellia@gmail.com>
2024-11-06 11:43:10 +0700

    TUTORIAL 7 IN PROGRESS

---
+++ lib/widgets/mood_card.dart
@@ -0,0 +1,67 @@
+// lib/widgets/mood_card.dart
+import 'package:flutter/material.dart';
+import '../screens/moodentry_form.dart'; // Import MoodEntryFormPage from the new screens directory
+
+class ItemCard extends StatelessWidget {
+  // Display the card with an icon and name.
+
+  final ItemHomepage item; 
+  
+  const ItemCard(this.item, {super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      // Specify the background color of the application theme.
+      color: Theme.of(context).colorScheme.secondary,
+      // Round the card border.
+      borderRadius: BorderRadius.circular(12),
+      
+      child: InkWell(
+        // Action when the card is pressed.
+        onTap: () {
+          // Display the SnackBar message when the card is pressed.
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("You have pressed the ${item.name} button!")));
+          // Navigate to the appropriate route (depending on the button type)
+          if (item.name == "Add Mood") {
+            // TODO: Use Navigator.push to navigate to a MaterialPageRoute that includes TrackerFormPage.
+          }
+        },
+        // Container to store the Icon and Text
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              // Place the Icon and Text in the center of the card.
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  
+}
+
+class ItemHomepage {
+     final String name;
+     final IconData icon;
+
+     ItemHomepage(this.name, this.icon);
+ }
